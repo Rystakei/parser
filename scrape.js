73d1620f0f6b16ee24a37f14d2d0c837ac83bee1
Krista Williams <rystakei@gmail.com>
2016-02-13 22:13:21 -0500

    Added bluebird and request-promise libraries

--- scrape.js
+++ scrape.js
@@ -1,76 +1,74 @@
-var request = require('request'),
+var q = require('bluebird'),
+    request = require('request-promise'),
 		cheerio = require('cheerio'),
 		fs = require('fs'),
 		jsonFile = require('jsonfile'),
-    _ = require("lodash"),
-    $ = require('jquery');
-
-var url = 'http://lyricstranslate.com/en/ah-w-noss-%D8%A2%D9%87-%D9%88-%D9%86%D8%B5.html',
-    tokens = [];
-
-
-
+    _ = require("lodash");
+    
 var pages = [
              {url: 'http://lyricstranslate.com/en/ah-w-noss-%D8%A2%D9%87-%D9%88-%D9%86%D8%B5.html', selector: '#content00 p'},
              {url: 'http://lyricstranslate.com/en/request/ya-tabtab-wa-dala3-2', selector: '#full-text'}
-            ];
+            ],
+    tokens = [];
 
+var newScrapePage = function($, url, selector) {
+  console.log("scraping starts");
+  var title = $('.title-h2').first().text(), 
+      selectedElements = $(selector);
 
+  selectedElements.each(function(i, element) {
+    var lyrics = $(element).text(),
+        splitLyrics = lyrics.split(" ");
 
-var scrapePage = function(url, selector) {
-  request(url, function (error, response, html) {
-    if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
-      
+    $(splitLyrics).each(function(i, token) {
+      var existingToken = _.find(tokens, function(obj) { return obj.name == token});
+      if (existingToken) {
+        existingToken["count"]= existingToken["count"] + 1;
 
-      var title = $('.title-h2').first().text(), 
-          selectedElements = $(selector);
+        if (existingToken.songs.indexOf(title) === -1){
+          existingToken.songs.push(title);
+          // console.log("tokens", tokens);
+        }
 
-      selectedElements.each(function(i, element) {
-        var lyrics = $(element).text(),
-            splitLyrics = lyrics.split(" ");
+      }
+      else {
+        tokens.push({name: token, count: 1, songs: [title]});
+      }
 
-        $(splitLyrics).each(function(i, token) {
-          var existingToken = _.find(tokens, function(obj) { return obj.name == token});
-          if (existingToken) {
-            existingToken["count"]= existingToken["count"] + 1;
-
-            if (existingToken.songs.indexOf(title) === -1){
-              existingToken.songs.push(title);
-              // console.log("tokens", tokens);
-            }
-
-          }
-          else {
-            tokens.push({name: token, count: 1, songs: [title]});
-          }
-
-      var sortedTokens = _.sortBy(tokens, function(o) { return o.count; }).reverse(),
-          mostCommonAmongSongs = _.filter(tokens, function(o) {
-            return o.songs.length > 1; }).reverse();
-      // console.log("sortedResults: " , sortedTokens);
-
-      console.log("\n \n Appears in Multiple Songs: ", mostCommonAmongSongs);
-
-          
-        });
-      });
-
-
-    }
+    });
   });
+};
 
-}
+var requests = [];
 
 pages.forEach(function(page) {
-  scrapePage(page.url, page.selector);
+  var options = {
+    uri: page.url,
+    transform: function(body){
+      return cheerio.load(body);
+    }
+  };
+
+  var pageRequest = request(options)
+                      .then(function($) {
+                        newScrapePage($, page.url, page.selector);
+                      });
+  requests.push(pageRequest);
 });
 
+q.all(requests).then(function(results){
+  console.log('all done with ' + results.length + ' queries');
 
+  var sortedTokens = _.sortBy(tokens, function(o) { return o.count; }).reverse(),
+      mostCommonAmongSongs = _.filter(tokens, function(o) {
+        return o.songs.length > 1; }).reverse();
+  // console.log("sortedResults: " , sortedTokens);
 
+  console.log("\n \n Appears in Multiple Songs: ", mostCommonAmongSongs);
 
+      
 
-
+});
 
 
 
